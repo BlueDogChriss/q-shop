eddfdf5b213e5ca9f54b5f79a8941d03c463c5b2
Cristian Munteanu <munteanu.cristian1009@gmail.com>
2023-11-01 17:23:22 +0200

    updated user register validations

--- backend/controllers/validations/user.js
+++ backend/controllers/validations/user.js
@@ -1,12 +1,25 @@
 const UserModel = require('../../models').User
 
-let validate ={
+let validate = {
 
-    register: async (object) =>{
+    register: async (object) => {
         let errors = {};
-        if(!object.name||!object.surname||!object.email||!object.phone||!object.password){
+        if (!object.name || !object.surname || !object.email || !object.phone || !object.password) {
             errors.emptyFields = "One or many fields must not be empty";
         }
+        if (!object.name.match(/^(?!-)(?!.*--)[A-Za-z-]+(?<!-)$/)
+            || !object.surname.match(/^(?!-)(?!.*--)[A-Za-z-]+(?<!-)$/)) {
+            errors.invalidInput = "Name and Surname must only contain letters"
+        }
+        if (!object.email.includes("@gmail.com")) {
+            errors.invalidEmail = "Invalid email input, email can only be of @gmail type"
+        } else {
+            const existentEmail = await UserModel.findOne({ where: { email: object.email } });
+            if (existentEmail) {
+                errors.existentEmail = "This email is already registered";
+            }
+        }
+        return errors;
     }
 }
 
