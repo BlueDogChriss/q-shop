7ba3118f08aa345b3875f4d470f555cd41b17921
Cristian Munteanu <munteanu.cristian1009@gmail.com>
2024-06-02 16:00:28 +0300

    starte developing Products components for ecomm site, implemented types for product, images, size and color

---
+++ ecomm-store/actions/get-products.tsx
@@ -0,0 +1,30 @@
+import { Product } from "@/types";
+
+import qs from "query-string"
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`
+
+interface Querry{
+    categoryId?: string;
+    colorId?: string;
+    sizeId?: string;
+    isFeatured?: boolean;
+}
+
+const getProducts = async (querry: Querry): Promise<Product[]> => {
+    const url = qs.stringifyUrl({
+        url: URL,
+        query: {
+            colorId: querry.colorId,
+            sizeId: querry.sizeId,
+            categoryId: querry.categoryId,
+            isFeatured: querry.isFeatured,
+        },
+    });
+    
+    const res = await fetch(URL);
+
+    return res.json();
+}
+
+export default getProducts;
--- ecomm-store/app/(routes)/page.tsx
+++ ecomm-store/app/(routes)/page.tsx
@@ -1,17 +1,22 @@
 import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/Billboard/billboard";
 import Container from "@/components/ui/container";
-
+import ProductList from "@/components/Products/product-list";
 
 
 const HomePage = async () => {
 
     const billboard = await getBillboard("2d1241c1-bbea-47f8-9fcc-01e466b5a749")
+    const products = await getProducts({isFeatured: true})
 
     return ( 
         <Container>
             <div className="space-y-10 pb-10">
                 <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products}/>
             </div>
         </Container>
      );
