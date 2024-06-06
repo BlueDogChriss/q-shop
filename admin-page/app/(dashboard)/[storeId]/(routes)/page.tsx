7abb765c012faf1b6dc7672b50c02ec9cb6b5f3c
Cristian Munteanu <munteanu.cristian1009@gmail.com>
2024-06-06 21:57:22 +0300

    added actions for checkout, products and cart manipulation, modified hooks and custom hooks implemented stripe checkout

--- admin-page/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ admin-page/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,9 @@
+import { Heading } from "@/components/Heading/heading";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+import { DollarSign } from "lucide-react";
 
 interface DashboardPageProps{
     params: {
@@ -10,15 +15,29 @@ interface DashboardPageProps{
 const DashboardPage: React.FC<DashboardPageProps> = async({
     params
 }) => {
-    const store = await prismadb.store.findFirst({
-        where: {
-            id: params.storeId
-        }
-    });
+
 
     return (
-        <div>
-            Active Store: {store?.name}
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <Heading title="Dashboard" description="Overvoew of your store" />
+                <Separator/>
+                <div className="grid gap-4 grid-cols-3">
+                    <Card>
+                        <CardHeader className="pb-2 flex flex-row items-center justify-between space-y-0">
+                            <CardTitle className="text-sm font-medium">
+                            Total revenue
+                            </CardTitle>
+                        <DollarSign className="h-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold">
+                                {formatter.format(100)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
--- ecomm-store/app/layout.tsx
+++ ecomm-store/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 
 import  Footer from "@/components/Footer/footer"
 import Navbar from "@/components/Navbar/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Roboto({
   weight: '400',
@@ -23,6 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider/>
+        <ToastProvider/>
         <Navbar/>
         {children}
         <Footer/>
